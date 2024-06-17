45ddfef12f9b0b118ced863deac4d8458e455710
Phuc Tran <glup3.tran@gmail.com>
2024-06-17 18:41:43 +0200

    add transparent window

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,33 @@
 return {
   {
-    "shatur/neovim-ayu",
+    {
+      "shatur/neovim-ayu",
+      config = function()
+        require("ayu").setup({
+          overrides = {
+            Normal = { bg = "None" },
+            ColorColumn = { bg = "None" },
+            SignColumn = { bg = "None" },
+            Folded = { bg = "None" },
+            FoldColumn = { bg = "None" },
+            CursorLine = { bg = "None" },
+            CursorColumn = { bg = "None" },
+            WhichKeyFloat = { bg = "None" },
+            VertSplit = { bg = "None" },
+          },
+        })
+      end,
+    },
 
     {
-      "folke/tokyonight.nvim",
-      enabled = true,
+      "rebelot/kanagawa.nvim",
     },
 
     {
       "LazyVim/LazyVim",
       opts = {
         colorscheme = "ayu-dark",
+        -- colorscheme = "kanagawa",
         overrides = {},
       },
     },
